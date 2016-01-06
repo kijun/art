f2d55459d07c40a50364f896b62a4717c7ab4824
Kijun Seo <kijunseo@gmail.com>
2016-01-06 17:04:20 +0900

    render new sectors based on ship location

--- Assets/Scripts/RenderSectors.cs
+++ Assets/Scripts/RenderSectors.cs
@@ -15,10 +15,10 @@ public class RenderSectors : MonoBehaviour {
 
     public GameObject planetPrefab;
 
-
     PlanetarySystem sys = new PlanetarySystem();
     Transform player;
-
+    Sector prevSector;
+    HashSet<Sector>renderedSectors = new HashSet<Sector>();
 
 	// Use this for initialization
 
@@ -30,14 +30,27 @@ public class RenderSectors : MonoBehaviour {
                 RenderSector(i, j);
             }
         }
+        prevSector = sys.GetSector(0, 0);
     }
 
-    Coord CurrentSector() {
+    Sector CurrentSector() {
         var ssize = Consts.sectorSize;
-        Vector2 pos = transform.position;
-        int x = Mathf.FloorToInt(pos.x - ssize/2.0f);
-        int y = Mathf.FloorToInt(pos.y - ssize/2.0f);
-        return new Coord(x, y);
+        Vector2 pos = player.position;
+        int x = Mathf.FloorToInt((pos.x - ssize/2.0f)/ssize);
+        int y = Mathf.FloorToInt((pos.y - ssize/2.0f)/ssize);
+        Debug.Log("For pos: " + pos + "Current Sector (" + x + ", " + y + ")");
+        return sys.GetSector(x, y);
+    }
+
+    HashSet<Sector> AdjacentSectors() {
+        var sectors = new HashSet<Sector>();
+        var cs = CurrentSector();
+        for (int i = -1; i<=1; i++) {
+            for (int j = -1; j<=1; j++) {
+                sectors.Add(sys.GetSector(cs.col+i, cs.row+j));
+            }
+        }
+        return sectors;
     }
 
     void RenderSector(int i, int j) {
@@ -47,10 +60,28 @@ public class RenderSectors : MonoBehaviour {
             var pgo = Instantiate(planetPrefab);
             pgo.transform.position = p.origin;
         }
+        renderedSectors.Add(sector);
+    }
+
+    void RemoveSector(int i, int j) {
     }
 
 	// Update is called once per frame
 	void Update () {
-        // if current sector is different then unfold, recreate
 	}
+
+    void FixedUpdate () {
+        // if current sector is different then unfold, recreate
+        var currSector = CurrentSector();
+        if (currSector != prevSector) {
+            foreach (var s in AdjacentSectors()) {
+                if (!renderedSectors.Contains(s)) {
+                    RenderSector(s.col, s.row);
+                    renderedSectors.Add(s);
+                }
+            }
+            prevSector = currSector;
+        }
+        // TODO: remove sectors
+    }
 }
