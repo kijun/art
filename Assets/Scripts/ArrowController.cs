2db6630a90e17ec3ead6d8ce44dbaad394375edf
Kijun Seo <kijunseo@gmail.com>
2016-02-18 01:05:15 +0900

    arrow pattern

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PolygonCollider2D), typeof (Rigidbody2D))]
+
+public class ArrowController : MonoBehaviour {
+    // every few seconds, turn direction?
+    // or turn direction but have the same velocity
+    // until few seconds later when it must stop
+    //
+    public float baseSpeed = 1f;
+    public float chaseTime = 3f;
+    private Rigidbody2D rgbd;
+    private Transform player;
+
+    void Awake() {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    public void ChasePlayer(Transform aPlayer) {
+        player = aPlayer;
+        StartCoroutine(DoChase());
+    }
+
+    IEnumerator DoChase() {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < chaseTime) {
+            elapsedTime += Time.deltaTime;
+            var direction = player.position - transform.position;
+            rgbd.velocity = direction.normalized * baseSpeed;
+            //rgbd.rotation = 180f - Mathf.Atan2(-2, 2) / (2 * Mathf.PI) * 360f;
+            rgbd.rotation = -90f + Mathf.Atan2(direction.y, direction.x) / (2 * Mathf.PI) * 360f;
+            Debug.Log("y"+ direction.y +"x"+ direction.x);
+            Debug.Log(Mathf.Atan2(direction.y, direction.x));
+            yield return null;
+        }
+    }
+}
+
---
+++ Assets/Scripts/ArrowObstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowObstacle : MonoBehaviour {
+
+    public ArrowController arrowPrefab;
+    public float secondsToSpawn = 3f;
+    private PlayerController playerCtrl;
+
+	// Use this for initialization
+	void Awake() {
+        playerCtrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        StartCoroutine(SpawnArrow());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    IEnumerator SpawnArrow() {
+        while (true) {
+            var arrow = Instantiate<ArrowController>(arrowPrefab);
+            arrow.ChasePlayer(playerCtrl.transform);
+            yield return new WaitForSeconds(secondsToSpawn);
+        }
+    }
+}
+
