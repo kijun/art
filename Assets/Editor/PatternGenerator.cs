fddbe750bdeb5e9b4571a741d4f51edc113aebb8
Kijun Seo <kijunseo@gmail.com>
2016-02-25 16:38:23 +0900

    add pattern scripts

--- Assets/Editor/PatternGenerator.cs
+++ Assets/Editor/PatternGenerator.cs
@@ -11,9 +11,10 @@ public static class PatternGenerator {
     const float gapBetweenPatterns = cameraWidth*2;
     const string patternDirPath = "Assets/Patterns";
     const string patternBackgroundPath = "Assets/Prefabs/PatternBackground.prefab";
+    const string playerPrefabPath = "Assets/Prefabs/Player.prefab";
 
 	// Add menu named "My Window" to the Window menu
-	[MenuItem ("GameObject/Create New Pattern %&p")]
+	[MenuItem ("GameObject/Create New Pattern %&n")]
 	public static void CreatePattern () {
         // count the number of patterns in folder
         var dir = new DirectoryInfo(patternDirPath);
@@ -24,15 +25,14 @@ public static class PatternGenerator {
 
         int patNum = info.Length + 1;
         var go = new GameObject("Pattern"+ patNum);
-        float goXCenter = (cameraWidth + gapBetweenPatterns) * patNum;
-        go.transform.position = new Vector3(goXCenter, 0, 0);
+        go.transform.position = new Vector3(CenterX(patNum), 0, 0);
 
-        var background = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(patternBackgroundPath));
+        var background = InstantiateFromPath(patternBackgroundPath);
         Debug.Log(AssetDatabase.LoadAssetAtPath<GameObject>(patternBackgroundPath));
         Debug.Log(background);
         background.transform.localScale = new Vector2(cameraWidth, cameraHeight*2);
         background.transform.SetParent(go.transform);
-        background.transform.localPosition = new Vector2(0, cameraHeight);
+        background.transform.localPosition = new Vector3(0, cameraHeight, 100); // clip
 
         /*
         var bounds = new Bounds(
@@ -49,6 +49,24 @@ public static class PatternGenerator {
 		window.Show();
         */
 	}
+
+    [MenuItem ("Assets/Play Pattern %&p")]
+    public static void PlayPattern() {
+        var pattern = Selection.activeGameObject;
+        var startPos = pattern.transform.position;
+        Camera.main.transform.position = new Vector3(startPos.x, cameraHeight/2, -10);
+        var player = GameObject.FindWithTag("Player");
+        player.transform.position = startPos.IncrY(0.5f);
+        EditorApplication.isPlaying = true;
+    }
+
+    public static float CenterX(int patNum) {
+        return (cameraWidth + gapBetweenPatterns) * patNum;
+    }
+
+    public static GameObject InstantiateFromPath(string path) {
+        return (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(path));
+    }
 }
 
 public class PatternController : MonoBehaviour {
