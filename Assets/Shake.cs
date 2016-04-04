502f91a65ad163ed9c188e3a499b3eb730e39ab6
Kijun Seo <kijunseo@gmail.com>
2016-04-04 14:37:33 +0900

    the first cause argument

---
+++ Assets/Shake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+
+public class Shake : MonoBehaviour {
+
+    private float period;
+    private float range;
+    private float fixedAngle;
+
+	// Use this for initialization
+	void Start () {
+        period = Random.Range(0.03f, 0.5f);
+        fixedAngle = Random.Range(0f, 360f);
+        range = Random.Range(30f,135f);
+        StartCoroutine(Oscillate());
+	}
+
+    IEnumerator Oscillate() {
+        float sign = 1;
+        while (true) {
+            //GetComponent<Rigidbody2D>().angularVelocity = sign * 1000;
+            transform.eulerAngles = new Vector3(0, 0, fixedAngle + range *sign);
+            sign *= -1;
+            yield return new WaitForSeconds(period);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
