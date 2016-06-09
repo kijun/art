00c25843f36a7589505afad8bf294b5cdb5f5415
Kijun Seo <kijunseo@gmail.com>
2016-06-09 18:10:50 +0900

    clean up

--- Assets/Scripts/Legacy/PathController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PathController : MonoBehaviour {
-    public float speed = 2;
-    public float angularVelocity = 15;
-    public PathComponent[] path;
-}
-
--- Assets/Scripts/Pattern/Model/BasePattern.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System;
-
-//public abstract class BasePattern : ScriptableObject {
-public abstract class BasePattern {//: ScriptableObject {
-    public abstract Type controllerType { get; }
-}
-
