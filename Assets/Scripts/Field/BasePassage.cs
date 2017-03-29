08e7d55e8b5cd97827ddba4b8091f554f724806d
Kijun Seo <kijunseo@gmail.com>
2017-03-29 18:53:41 +0900

    Get base passage to work

--- Assets/Scripts/Field/BasePassage.cs
+++ Assets/Scripts/Field/BasePassage.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 
 public enum CellType {
+    None = 0,
     // lets try to use two args to define all
-    Velocity,
-    Rotation,
-    Scale,
-    Color,
-    Destruction
+    Velocity = 10,
+    Rotation = 20,
+    Scale = 30,
+    Color = 40,
+    Destruction = 90
 }
+
+[System.Serializable]
 public struct CellDefinition {
     public float startTime;
     public float endTime;
@@ -29,17 +32,18 @@ public class BasePassage : MonoBehaviour {
     public float startTime;
     public float endTime;
     public Range timeBetweenActivation;
+    // start location?
 
     /* Creation logic */
-
-    public Animatable objectPrefab;
+    public Animatable2 objectPrefab;
     public Range objectHeight;
     public Range objectWidth;
     public Range objectInitialAngle;
     public Range objectsPerActivation = new Range(1);
+    public Range objectEntryAngle;
+    public Vector2 objectPositionRange;
 
-    /* */
-
+    /* Passage Cells */
     public CellDefinition[] cells;
 
 	// Use this for initialization
@@ -51,11 +55,65 @@ public class BasePassage : MonoBehaviour {
         // create
         yield return new WaitForSeconds(startTime);
         while (endTime < float.Epsilon || Time.time < endTime) {
-            var numObj = Mathf.RoundToInt(objPerActivation.RandomValue());
-            for (int i = 0; i < numObj; i++) {
-                StartCoroutine(CreateAndMoveObject());
-            }
+            var numObj = Mathf.RoundToInt(objectsPerActivation.RandomValue());
+            StartCoroutine(CreateAndMoveObjects(numObj));
             yield return new WaitForSeconds(timeBetweenActivation.RandomValue());
+        }
+    }
+
+    IEnumerator CreateAndMoveObjects(int count) {
+        var objects = new Animatable2[count];
+
+        // Create Objects
+        for (int i = 0; i<count; i++) {
+            var width = objectWidth.RandomValue();
+            var height = objectHeight.RandomValue();
+            var entryAngle = objectEntryAngle.RandomValue();
+            var distanceFromOrigin = cameraDiameter / 2 + Mathf.Sqrt(width * width + height * height) / 2;
+            var objectPos =
+                Quaternion.Euler(0, 0, entryAngle) *
+                (Vector2.up * distanceFromOrigin +
+                 new Vector2(objectPositionRange.x * Random.value - objectPositionRange.x/2,
+                             objectPositionRange.y * Random.value - objectPositionRange.y/2));
+
+            var rotation = Quaternion.Euler(0, 0, entryAngle);
+            objects[i] = GameObject.Instantiate<Animatable2>(objectPrefab, objectPos, rotation);
+        }
+
+
+        foreach (var cell in cells) {
+            StartCoroutine(RunCell(cell, objects));
+        }
+
+        yield return true;
+    }
+
+    IEnumerator RunCell(CellDefinition cell, Animatable2[] objects) {
+        yield return new WaitForSeconds(cell.startTime);
+        // probably multiple methods
+        foreach (var obj in objects) {
+            switch (cell.type) {
+                case CellType.Velocity:
+                    obj.velocity = new Vector2(cell.arg1.RandomValue(), cell.arg2.RandomValue());
+                    break;
+            }
+        }
+        /*
+        var c = target.GetComponent<SpriteRenderer>().material.color;
+        c.a = Random.Range(0.3f, 0.8f);
+        target.GetComponent<SpriteRenderer>().material.color = c;
+
+        */
+    }
+
+    float cameraDiameter {
+        get {
+            var inGameHeight = Camera.main.orthographicSize * 2;
+            var inGameWidth = (float)Screen.width / (float)Screen.height * inGameHeight;
+
+            var cameraDiameter = Mathf.Sqrt(inGameHeight * inGameHeight + inGameWidth * inGameWidth);
+
+            return cameraDiameter;
         }
     }
 }
